e7674307058194bffea7dbee45dd49613dfac01e
Ramaaprilliant <ramaaprilliant@gmail.com>
2020-08-03 15:45:26 +0100

    finish the regroup in the cv HTML

--- cv/views.py
+++ cv/views.py
@@ -3,5 +3,5 @@ from django.utils import timezone
 from .models import Listing
 
 def cv_list(request):
-    posts = Listing.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'cv/cv_list.html', {'cv': posts})
+    cvlists = Listing.objects.all()
+    return render(request, 'cv/cv_list.html', {'cvlists': cvlists})
